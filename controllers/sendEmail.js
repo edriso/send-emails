2f44023a80e789c9ff9c2abe38c3a69367e341b7
edriso <edriso.dev@gmail.com>
2023-09-17 11:57:10 +0300

    send emails using ethereal

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,5 +1,25 @@
+const nodemailer = require('nodemailer');
+
 const sendEmail = async (req, res) => {
-  res.send('hey');
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: Number(process.env.EMAIL_PORT),
+    secure: process.env.EMAIL_SECURE === 'true',
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Fred Boo 👻" <boo@example.com>',
+    to: 'bear@example.com, baz@example.com',
+    subject: 'Hello ✔',
+    // text: 'Hello world!',
+    html: '<b>Hello world!</b>',
+  });
+
+  res.json(info);
 };
 
 module.exports = sendEmail;
